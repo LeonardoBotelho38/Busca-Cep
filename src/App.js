a794e1304c8cc833bca7127a03a22da1dd3787d6
Leonardo Botelho <109173085+LeonardoBotelho38@users.noreply.github.com>
2023-02-15 19:20:16 -0300

    teste de buscador de cep

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+const [endereco, setEndereco] = useState('')
+const [resposta, setResposta] = useState('')
+
+function buscaEndereco(evento){
+  const cep = evento.target.value
+  setEndereco({
+    cep
+  })
+    if(cep && cep.length === 8){
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(resposta => resposta.json())
+      .then(dados =>{
+        setEndereco({
+          cep:dados.cep,
+          rua: dados.logradouro,
+          bairro: dados.bairro,
+          cidade: dados.localidade,
+          estado: dados.uf
+        })
+      })
+    } 
+  }
+    function mostraEndereco(endereco){
+     
+      return ( <ul>
+        <li>CEP: {endereco.cep}</li>
+        <li>Rua: {endereco.rua}</li>
+        <li>Bairro: {endereco.bairro}</li>
+        <li>Cidade: {endereco.cidade}</li>
+        <li>Estado: {endereco.estado}</li>
+        
+      </ul> )
+      
+    }
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Busca CEP</h1>
+      <input placeholder='digite o seu cep' type="text" onChange={buscaEndereco} />
+      <button onClick={()=> setResposta(mostraEndereco(endereco))}>buscar</button>
+     {resposta}
+     
+      
       </header>
     </div>
   );
